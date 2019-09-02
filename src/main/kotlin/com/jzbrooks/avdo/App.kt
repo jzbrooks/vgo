b215fea639672141adeb04576d65048cb61d2bb4
Justin Brooks <jstnz72@gmail.com>
2019-09-01 21:06:08 -0500

    Add reading and writing (unprocessed) to command line args

--- src/main/kotlin/com/jzbrooks/avdo/App.kt
+++ src/main/kotlin/com/jzbrooks/avdo/App.kt
@@ -1,5 +1,24 @@
 package com.jzbrooks.avdo
 
-fun main() {
+import com.jzbrooks.avdo.vd.parse
+import com.jzbrooks.avdo.vd.write
+import java.io.ByteArrayOutputStream
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
+fun main(args: Array<String>) {
+    val (path, target) = args
+
+    FileInputStream(path).use { inputStream ->
+        val reader = parse(inputStream)
+
+        ByteArrayOutputStream().use {
+            write(reader, it)
+            println(it.toString())
+        }
+
+        FileOutputStream(target).use {
+            write(reader, it)
+        }
+    }
 }
