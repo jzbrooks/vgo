bdf7cecbf1c7435b9cae3dd72031f39388e0f66c
Justin Brooks <jstnz72@gmail.com>
2019-09-06 22:08:12 -0500

    Support implicit commands

--- src/main/kotlin/com/jzbrooks/avdo/graphic/command/Parser.kt
+++ src/main/kotlin/com/jzbrooks/avdo/graphic/command/Parser.kt
@@ -10,29 +10,33 @@ inline class CommandString(val data: String) {
                 .map { command ->
                     when {
                         command.startsWith("M") -> {
-                            val data = pathData.findAll(command)
-                                    .map { it.value.toInt() }
-                                    .zipWithNext { a, b -> Pair(a, b) }
+                            val data = argumentPairs.findAll(command)
+                                    .map { match ->
+                                        val components = match.value.split(Regex("[,\\s]"))
+                                        components[0].toInt() to components[1].toInt()
+                                    }
                                     .toList()
                             MoveTo(data)
                         }
                         command.startsWith("L") -> {
-                            val data = pathData.findAll(command)
-                                    .map { it.value.toInt() }
-                                    .zipWithNext { a, b -> Pair(a, b) }
+                            val data = argumentPairs.findAll(command)
+                                    .map { match ->
+                                        val components = match.value.split(Regex("[,\\s]"))
+                                        components[0].toInt() to components[1].toInt()
+                                    }
                                     .toList()
 
                             LineTo(data)
                         }
                         command.startsWith("V") -> {
-                            val data = pathData.findAll(command)
+                            val data = arguments.findAll(command)
                                     .map { it.value.toInt() }
                                     .toList()
 
                             VerticalLineTo(data)
                         }
                         command.startsWith("H") -> {
-                            val data = pathData.findAll(command)
+                            val data = arguments.findAll(command)
                                     .map { it.value.toInt() }
                                     .toList()
 
@@ -45,7 +49,9 @@ inline class CommandString(val data: String) {
     }
 
     companion object {
-        val commandRegex = Regex("(?=[MmLlHhVvCcSsQqTtAaZz])\\s*")
-        val pathData = Regex("[-+]?(?:\\d*\\.\\d+|\\d+\\.?)([eE][-+]?\\d+)?")
+        private val commandRegex = Regex("(?=[MmLlHhVvCcSsQqTtAaZz])\\s*")
+        private val number = Regex("[-+]?(?:\\d*\\.\\d+|\\d+\\.?)([eE][-+]?\\d+)?")
+        private val arguments = Regex("$number")
+        private val argumentPairs = Regex("$number[,\\s]$number")
     }
 }
--- src/test/kotlin/com/jzbrooks/avdo/graphic/command/ParserTests.kt
+++ src/test/kotlin/com/jzbrooks/avdo/graphic/command/ParserTests.kt
@@ -6,14 +6,111 @@ import assertk.assertions.isInstanceOf
 import kotlin.test.Test
 
 class ParserTests {
-    @Test
-    fun testParseMultipleCommandInstructions() {
-        val moveToString = "M1,1 L2,5 V3 Z"
 
-        val commands = CommandString(moveToString).toCommandList()
+    @Test
+    fun testParseCompactCommandString() {
+        val pathCommandString = "M1,1L2,5V3Z"
+
+        val commands = CommandString(pathCommandString).toCommandList()
 
         assertThat(commands[0])
                 .isEqualTo(MoveTo(listOf(1 to 1)))
+        assertThat(commands[1])
+                .isEqualTo(LineTo(listOf(2 to 5)))
+        assertThat(commands[2])
+                .isEqualTo(VerticalLineTo(listOf(3)))
+        assertThat(commands[3])
+                .isInstanceOf(ClosePath::class.java)
+    }
+
+    @Test
+    fun testParseLooseCommandString() {
+        val pathCommandString = "M 1 1 L 2 5 V 3 Z"
+
+        val commands = CommandString(pathCommandString).toCommandList()
+
+        assertThat(commands[0])
+                .isEqualTo(MoveTo(listOf(1 to 1)))
+        assertThat(commands[1])
+                .isEqualTo(LineTo(listOf(2 to 5)))
+        assertThat(commands[2])
+                .isEqualTo(VerticalLineTo(listOf(3)))
+        assertThat(commands[3])
+                .isInstanceOf(ClosePath::class.java)
+    }
+
+    @Test
+    fun testParseCommaSeparatedPairsWithoutImplicitCommands() {
+        val pathCommandString = "M1,1 L2,5 V3 Z"
+
+        val commands = CommandString(pathCommandString).toCommandList()
+
+        assertThat(commands[0])
+                .isEqualTo(MoveTo(listOf(1 to 1)))
+        assertThat(commands[1])
+                .isEqualTo(LineTo(listOf(2 to 5)))
+        assertThat(commands[2])
+                .isEqualTo(VerticalLineTo(listOf(3)))
+        assertThat(commands[3])
+                .isInstanceOf(ClosePath::class.java)
+    }
+
+    @Test
+    fun testParseCommaSeparatedPairsWithImplicitCommands() {
+        val pathCommandString = "M1,1 1,2 L2,5 V3 Z"
+
+        val commands = CommandString(pathCommandString).toCommandList()
+
+        assertThat(commands[0])
+                .isEqualTo(MoveTo(listOf(1 to 1, 1 to 2)))
+        assertThat(commands[1])
+                .isEqualTo(LineTo(listOf(2 to 5)))
+        assertThat(commands[2])
+                .isEqualTo(VerticalLineTo(listOf(3)))
+        assertThat(commands[3])
+                .isInstanceOf(ClosePath::class.java)
+    }
+
+    @Test
+    fun testParseSpaceSeparatedPairsWithoutImplicitCommands() {
+        val pathCommandString = "M1 1 L2 5 V3 Z"
+
+        val commands = CommandString(pathCommandString).toCommandList()
+
+        assertThat(commands[0])
+                .isEqualTo(MoveTo(listOf(1 to 1)))
+        assertThat(commands[1])
+                .isEqualTo(LineTo(listOf(2 to 5)))
+        assertThat(commands[2])
+                .isEqualTo(VerticalLineTo(listOf(3)))
+        assertThat(commands[3])
+                .isInstanceOf(ClosePath::class.java)
+    }
+
+    @Test
+    fun testParseSpaceSeparatedPairsWithImplicitCommands() {
+        val pathCommandString = "M1 1 1 2 L2 5 V3 Z"
+
+        val commands = CommandString(pathCommandString).toCommandList()
+
+        assertThat(commands[0])
+                .isEqualTo(MoveTo(listOf(1 to 1, 1 to 2)))
+        assertThat(commands[1])
+                .isEqualTo(LineTo(listOf(2 to 5)))
+        assertThat(commands[2])
+                .isEqualTo(VerticalLineTo(listOf(3)))
+        assertThat(commands[3])
+                .isInstanceOf(ClosePath::class.java)
+    }
+
+    @Test
+    fun testParseMixedSeparatedPairsWithImplicitCommands() {
+        val pathCommandString = "M1,1 1 2 L2 5 V3 Z"
+
+        val commands = CommandString(pathCommandString).toCommandList()
+
+        assertThat(commands[0])
+                .isEqualTo(MoveTo(listOf(1 to 1, 1 to 2)))
         assertThat(commands[1])
                 .isEqualTo(LineTo(listOf(2 to 5)))
         assertThat(commands[2])
