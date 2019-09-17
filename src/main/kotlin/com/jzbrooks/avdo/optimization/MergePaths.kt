2cf5f3c5d7c940e73303ce31811de1cb76a062f9
Justin Brooks <jstnz72@gmail.com>
2019-09-17 18:25:04 -0500

    Handle mixed element types for graphic element path merges

--- src/main/kotlin/com/jzbrooks/avdo/optimization/MergePaths.kt
+++ src/main/kotlin/com/jzbrooks/avdo/optimization/MergePaths.kt
@@ -1,5 +1,6 @@
 package com.jzbrooks.avdo.optimization
 
+import com.jzbrooks.avdo.graphic.Element
 import com.jzbrooks.avdo.graphic.Graphic
 import com.jzbrooks.avdo.graphic.Group
 import com.jzbrooks.avdo.graphic.PathElement
@@ -12,8 +13,33 @@ class MergeGroupPaths : MergePaths(), Optimization<Group> {
 
 class MergeGraphicPaths : MergePaths(), Optimization<Graphic> {
     override fun visit(element: Graphic) {
-        // todo: we should apply these transformations in-place and only if the elements are adjacent in the list
-        element.elements = merge(element.elements.filterIsInstance<PathElement>())
+        // merge consecutive path elements of the same type
+        val elements = mutableListOf<Element>()
+        var currentChunk = mutableListOf<PathElement>()
+
+        fun process(item: Element) {
+            // merge previous (back to chunk start)
+            if (currentChunk.isNotEmpty()) elements.addAll(merge(currentChunk))
+
+            // add item
+            elements.add(item)
+
+            currentChunk = mutableListOf()
+        }
+
+        for (item in element.elements) {
+            if (item is PathElement) {
+                currentChunk.add(item)
+            } else {
+                process(item)
+            }
+        }
+
+        if (currentChunk.isNotEmpty()) {
+            elements.addAll(merge(currentChunk))
+        }
+
+        element.elements = elements
     }
 }
 
