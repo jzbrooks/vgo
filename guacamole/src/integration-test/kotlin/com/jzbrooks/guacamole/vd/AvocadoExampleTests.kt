33719d51079d6c168dd69b7ebd7c5df4cc6385d9
Justin Brooks <jstnz72@gmail.com>
2019-10-26 14:04:32 -0500

    Add a baseline test

--- guacamole/src/integration-test/kotlin/com/jzbrooks/guacamole/vd/AvocadoExampleTests.kt
+++ guacamole/src/integration-test/kotlin/com/jzbrooks/guacamole/vd/AvocadoExampleTests.kt
@@ -2,16 +2,18 @@ package com.jzbrooks.guacamole.vd
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import assertk.assertions.isLessThanOrEqualTo
 import com.jzbrooks.guacamole.App
 import org.junit.Test
 import java.io.File
 
 class AvocadoExampleTests {
-    private val avocadoExampleRelativePath = "src/integration-test/resources/avocado_example.xml"
-
+    private val avocadoExamplePath = "src/integration-test/resources/avocado_example.xml"
+    private val baselineAvocadoExamplePath = "src/integration-test/resources/baseline/avocado_example_optimized.xml"
     @Test
     fun testOptimizationFinishes() {
-        val arguments = arrayOf(avocadoExampleRelativePath, "-o", "build/integrationTest/test.xml")
+        val outputFilePath = "build/integrationTest/testOptimizationFinishes.xml"
+        val arguments = arrayOf(avocadoExamplePath, "-o", outputFilePath)
 
         val exitCode = App().run(arguments)
 
@@ -19,16 +21,27 @@ class AvocadoExampleTests {
     }
 
     @Test
-    fun testOptimizationIsCompact() {
-        val arguments = arrayOf(avocadoExampleRelativePath, "-o", "build/integrationTest/test.xml")
+    fun testOptimizedAssetIsEquivalentToBaseline() {
+        val outputFilePath = "build/integrationTest/testOptimizationIsCompact.xml"
+        val arguments = arrayOf(avocadoExamplePath, "-o", outputFilePath)
 
         App().run(arguments)
 
-        val content = File("build/integrationTest/test.xml").readText()
-        assertThat(content).isEqualTo(expected)
+        val content = File(outputFilePath).readText()
+        val baselineContent = File(baselineAvocadoExamplePath).readText()
+        assertThat(content).isEqualTo(baselineContent)
     }
 
-    companion object {
-        const val expected = """<vector xmlns:android="http://schemas.android.com/apk/res/android" android:height="108dp" android:viewportHeight="108" android:viewportWidth="108" android:width="108dp"><path android:fillColor="#26A69A" android:pathData="M0,0h108v108H0Z"/><path android:fillColor="#00000000" android:pathData="M-13.5,-27l0,162M1.5,-27l0,162m15,-162l0,162m15,-162l0,162m15,-162l0,162m15,-162l0,162M69,0l0,108M79,0l0,108M89,0l0,108M99,0l0,108m63,45L54,152.99998M162,143L54,142.99998M162,133L54,132.99998M162,123L54,122.99999M162,113L54.000008,112.99999M162,103L54.000008,102.99999M54,123l108,0M54,133l108,0M54,143l108,0M54,153l108,0M73,83l70,0M73,93l70,0M73,103l70,0M73,113l70,0M73,123l70,0M73,133l70,0M83,73l0,70M93,73l0,70M103,73l0,70M113,73l0,70M123,73l0,70M133,73l0,70" android:strokeColor="#33FFFFFF" android:strokeWidth="0.8"/></vector>"""
+    @Test
+    fun testOptimizedAssetIsNotLargerThanBaseline() {
+        val outputFilePath = "build/integrationTest/testOptimizedAssetIsNotLargerThanBaseline.xml"
+        val arguments = arrayOf(avocadoExamplePath, "-o", outputFilePath)
+
+        App().run(arguments)
+
+        val optimizedAssetSize = File(outputFilePath).length()
+        val baselineAssetSize = File("src/integration-test/resources/baseline/avocado_example_optimized.xml").length()
+
+        assertThat(optimizedAssetSize).isLessThanOrEqualTo(baselineAssetSize)
     }
 }
