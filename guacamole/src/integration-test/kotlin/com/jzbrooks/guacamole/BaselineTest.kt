d8abfeda8e921e3317a0ff664c1ddd2e28730cba
Justin Brooks <jstnz72@gmail.com>
2019-10-26 15:26:55 -0500

    Reorganize baseline integration tests

---
+++ guacamole/src/integration-test/kotlin/com/jzbrooks/guacamole/BaselineTest.kt
@@ -0,0 +1,45 @@
+package com.jzbrooks.guacamole
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isLessThanOrEqualTo
+import org.junit.Test
+import java.io.File
+import java.nio.file.Path
+
+abstract class BaselineTest(private val unoptimizedAsset: Path, private val baselineAsset: Path) {
+    @Test
+    fun testOptimizationFinishes() {
+        val outputFilePath = "build/integrationTest/visibility-strike/testOptimizationFinishes.xml"
+        val arguments = arrayOf(unoptimizedAsset.toString(), "-o", outputFilePath)
+
+        val exitCode = App().run(arguments)
+
+        assertThat(exitCode).isEqualTo(0)
+    }
+
+    @Test
+    fun testOptimizedAssetIsEquivalentToBaseline() {
+        val outputFilePath = "build/integrationTest/visibility-strike/testOptimizationIsCompact.xml"
+        val arguments = arrayOf(unoptimizedAsset.toString(), "-o", outputFilePath)
+
+        App().run(arguments)
+
+        val content = File(outputFilePath).readText()
+        val baselineContent = baselineAsset.toFile().readText()
+        assertThat(content).isEqualTo(baselineContent)
+    }
+
+    @Test
+    fun testOptimizedAssetIsNotLargerThanBaseline() {
+        val outputFilePath = "build/integrationTest/visibility-strike/testOptimizedAssetIsNotLargerThanBaseline.xml"
+        val arguments = arrayOf(unoptimizedAsset.toString(), "-o", outputFilePath)
+
+        App().run(arguments)
+
+        val optimizedAssetSize = File(outputFilePath).length()
+        val baselineAssetSize = baselineAsset.toFile().length()
+
+        assertThat(optimizedAssetSize).isLessThanOrEqualTo(baselineAssetSize)
+    }
+}
--- guacamole/src/integration-test/kotlin/com/jzbrooks/guacamole/vd/AvocadoExampleTests.kt
+++ guacamole/src/integration-test/kotlin/com/jzbrooks/guacamole/vd/AvocadoExampleTests.kt
@@ -1,47 +1,9 @@
 package com.jzbrooks.guacamole.vd
 
-import assertk.assertThat
-import assertk.assertions.isEqualTo
-import assertk.assertions.isLessThanOrEqualTo
-import com.jzbrooks.guacamole.App
-import org.junit.Test
-import java.io.File
+import com.jzbrooks.guacamole.BaselineTest
+import java.nio.file.Path
 
-class AvocadoExampleTests {
-    private val avocadoExamplePath = "src/integration-test/resources/avocado_example.xml"
-    private val baselineAvocadoExamplePath = "src/integration-test/resources/baseline/avocado_example_optimized.xml"
-    @Test
-    fun testOptimizationFinishes() {
-        val outputFilePath = "build/integrationTest/testOptimizationFinishes.xml"
-        val arguments = arrayOf(avocadoExamplePath, "-o", outputFilePath)
-
-        val exitCode = App().run(arguments)
-
-        assertThat(exitCode).isEqualTo(0)
-    }
-
-    @Test
-    fun testOptimizedAssetIsEquivalentToBaseline() {
-        val outputFilePath = "build/integrationTest/testOptimizationIsCompact.xml"
-        val arguments = arrayOf(avocadoExamplePath, "-o", outputFilePath)
-
-        App().run(arguments)
-
-        val content = File(outputFilePath).readText()
-        val baselineContent = File(baselineAvocadoExamplePath).readText()
-        assertThat(content).isEqualTo(baselineContent)
-    }
-
-    @Test
-    fun testOptimizedAssetIsNotLargerThanBaseline() {
-        val outputFilePath = "build/integrationTest/testOptimizedAssetIsNotLargerThanBaseline.xml"
-        val arguments = arrayOf(avocadoExamplePath, "-o", outputFilePath)
-
-        App().run(arguments)
-
-        val optimizedAssetSize = File(outputFilePath).length()
-        val baselineAssetSize = File(baselineAvocadoExamplePath).length()
-
-        assertThat(optimizedAssetSize).isLessThanOrEqualTo(baselineAssetSize)
-    }
-}
+class AvocadoExampleTests : BaselineTest(
+        Path.of("src/integration-test/resources/avocado_example.xml"),
+        Path.of("src/integration-test/resources/baseline/avocado_example_optimized.xml")
+)
