bd83a852452bccde90eded31539a768b41d2c8ba
Justin Brooks <jstnz72@gmail.com>
2019-11-09 19:54:35 -0600

    Avoid removing repeat relative commands

--- guacamole-core/src/main/kotlin/com/jzbrooks/guacamole/core/optimization/RemoveRedundantCommands.kt
+++ guacamole-core/src/main/kotlin/com/jzbrooks/guacamole/core/optimization/RemoveRedundantCommands.kt
@@ -4,6 +4,10 @@ import com.jzbrooks.guacamole.core.graphic.ContainerElement
 import com.jzbrooks.guacamole.core.graphic.Element
 import com.jzbrooks.guacamole.core.graphic.Graphic
 import com.jzbrooks.guacamole.core.graphic.PathElement
+import com.jzbrooks.guacamole.core.graphic.command.Command
+import com.jzbrooks.guacamole.core.graphic.command.CommandVariant
+import com.jzbrooks.guacamole.core.graphic.command.MoveTo
+import com.jzbrooks.guacamole.core.graphic.command.ParameterizedCommand
 
 class RemoveRedundantCommands : Optimization {
     override fun optimize(graphic: Graphic) {
@@ -22,12 +26,13 @@ class RemoveRedundantCommands : Optimization {
         val commandCount = pathElement.commands.size
 
         if (commandCount > 0) {
-            val commands = mutableListOf(pathElement.commands.first())
+            val commands = mutableListOf<Command>((pathElement.commands.first() as MoveTo).copy())
             for (current in pathElement.commands.slice(1 until commandCount)) {
-                val lastAdded = commands.last()
+                val lastAdded = commands.last() as? ParameterizedCommand<*>
 
-                // todo: might break the simple heart
-                if (lastAdded != current) commands.add(current)
+                if (lastAdded?.variant == CommandVariant.RELATIVE || lastAdded != current) {
+                    commands.add(current)
+                }
             }
 
             pathElement.commands = commands
