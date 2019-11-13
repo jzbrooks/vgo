8742f20b7bb0cdffc9ee3b70da5bc7b7eee76374
Justin Brooks <justinbrooks@fastmail.com>
2019-11-12 22:23:11 -0600

    Speed up path command parsing

--- guacamole-core/src/main/kotlin/com/jzbrooks/guacamole/core/graphic/command/Parser.kt
+++ guacamole-core/src/main/kotlin/com/jzbrooks/guacamole/core/graphic/command/Parser.kt
@@ -8,127 +8,147 @@ inline class CommandString(val data: String) {
                 .asSequence()
                 .filter { it.isNotBlank() }
                 .map { command ->
-                    val upperCommand = command.toUpperCase()
                     val variant = if (command[0].isLowerCase()) CommandVariant.RELATIVE else CommandVariant.ABSOLUTE
                     when {
-                        upperCommand.startsWith('M') -> {
-                            val parameters = points.findAll(command)
+                        command.startsWith('M', true) -> {
+                            val parameters = number.findAll(command)
+                                    .map { it.value }
+                                    .map(String::toFloat)
+                                    .chunked(2)
                                     .map(::mapPoint)
                                     .toList()
+
                             MoveTo(variant, parameters)
                         }
-                        upperCommand.startsWith('L') -> {
-                            val parameters = points.findAll(command)
+                        command.startsWith('L', true) -> {
+                            val parameters = number.findAll(command)
+                                    .map { it.value }
+                                    .map(String::toFloat)
+                                    .chunked(2)
                                     .map(::mapPoint)
                                     .toList()
 
                             LineTo(variant, parameters)
                         }
-                        upperCommand.startsWith('V') -> {
+                        command.startsWith('V', true) -> {
                             val parameters = number.findAll(command)
                                     .map { it.value.toFloat() }
                                     .toList()
 
                             VerticalLineTo(variant, parameters)
                         }
-                        upperCommand.startsWith('H') -> {
+                        command.startsWith('H', true) -> {
                             val parameters = number.findAll(command)
                                     .map { it.value.toFloat() }
                                     .toList()
 
                             HorizontalLineTo(variant, parameters)
                         }
-                        upperCommand.startsWith('Q') -> {
-                            val parameters = quadraticBezierCurveParameters.findAll(command)
+                        command.startsWith('Q', true) -> {
+                            val parameters = number.findAll(command)
+                                    .map { it.value }
+                                    .map(String::toFloat)
+                                    .chunked(4)
                                     .map(::mapQuadraticBezierCurveParameter)
                                     .toList()
 
                             QuadraticBezierCurve(variant, parameters)
                         }
-                        upperCommand.startsWith('T') -> {
-                            val parameters = points.findAll(command)
+                        command.startsWith('T', true) -> {
+                            val parameters = number.findAll(command)
+                                    .map { it.value }
+                                    .map(String::toFloat)
+                                    .chunked(2)
                                     .map(::mapPoint)
                                     .toList()
 
                             ShortcutQuadraticBezierCurve(variant, parameters)
                         }
-                        upperCommand.startsWith('C') -> {
-                            val parameters = cubicBezierCurveParameters.findAll(command)
+                        command.startsWith('C', true) -> {
+                            val parameters = number.findAll(command)
+                                    .map { it.value }
+                                    .map(String::toFloat)
+                                    .chunked(6)
                                     .map(::mapCubicBezierCurveParameter)
                                     .toList()
 
                             CubicBezierCurve(variant, parameters)
                         }
-                        upperCommand.startsWith('S') -> {
-                            val parameters = shortcutCubicBezierCurveParameters.findAll(command)
+                        command.startsWith('S', true) -> {
+                            val parameters = number.findAll(command)
+                                    .map { it.value }
+                                    .map(String::toFloat)
+                                    .chunked(4)
                                     .map(::mapShortcutCubicBezierCurveParameter)
                                     .toList()
 
                             ShortcutCubicBezierCurve(variant, parameters)
                         }
-                        upperCommand.startsWith('A') -> {
-                            val parameters = arcCurveParameters.findAll(command)
+                        command.startsWith('A', true) -> {
+                            val parameters = number.findAll(command)
+                                    .map { it.value }
+                                    .map(String::toFloat)
+                                    .chunked(7)
                                     .map(::mapEllipticalArcCurveParameter)
                                     .toList()
 
                             EllipticalArcCurve(variant, parameters)
                         }
-                        upperCommand.startsWith('Z') -> ClosePath()
+                        command.startsWith('Z', true) -> ClosePath()
                         else -> throw IllegalStateException("Expected one of $commandRegex but was $command")
                     }
                 }.toList()
     }
 
-    private fun mapPoint(match: MatchResult): Point {
-        val components = match.value.split(separator)
-        return Point(components[0].toFloat(), components[1].toFloat())
+    private fun mapPoint(components: List<Float>): Point {
+        return Point(components[0], components[1])
     }
 
-    private fun mapQuadraticBezierCurveParameter(match: MatchResult): QuadraticBezierCurve.Parameter {
-        val components = match.value.split(separator)
-        val control = Point(components[0].toFloat(), components[1].toFloat())
-        val end = Point(components[2].toFloat(), components[3].toFloat())
+    private fun mapQuadraticBezierCurveParameter(components: List<Float>): QuadraticBezierCurve.Parameter {
+        val control = Point(components[0], components[1])
+        val end = Point(components[2], components[3])
 
         return QuadraticBezierCurve.Parameter(control, end)
     }
 
-    private fun mapCubicBezierCurveParameter(match: MatchResult): CubicBezierCurve.Parameter {
-        val components = match.value.split(separator)
-        val startControl = Point(components[0].toFloat(), components[1].toFloat())
-        val endControl = Point(components[2].toFloat(), components[3].toFloat())
-        val end = Point(components[4].toFloat(), components[5].toFloat())
+    private fun mapCubicBezierCurveParameter(components: List<Float>): CubicBezierCurve.Parameter {
+        val startControl = Point(components[0], components[1])
+        val endControl = Point(components[2], components[3])
+        val end = Point(components[4], components[5])
 
         return CubicBezierCurve.Parameter(startControl, endControl, end)
     }
 
-    private fun mapShortcutCubicBezierCurveParameter(match: MatchResult): ShortcutCubicBezierCurve.Parameter {
-        val components = match.value.split(separator)
-        val endControl = Point(components[0].toFloat(), components[1].toFloat())
-        val end = Point(components[2].toFloat(), components[3].toFloat())
+    private fun mapShortcutCubicBezierCurveParameter(components: List<Float>): ShortcutCubicBezierCurve.Parameter {
+        val endControl = Point(components[0], components[1])
+        val end = Point(components[2], components[3])
 
         return ShortcutCubicBezierCurve.Parameter(endControl, end)
     }
 
-    private fun mapEllipticalArcCurveParameter(match: MatchResult): EllipticalArcCurve.Parameter {
-        val components = match.value.split(separator)
-        val radiusX = components[0].toFloat()
-        val radiusY = components[1].toFloat()
-        val angle = components[2].toFloat()
-        val arcFlag = if (components[3].toInt() == 1) EllipticalArcCurve.ArcFlag.LARGE else EllipticalArcCurve.ArcFlag.SMALL
-        val sweepFlag = if (components[4].toInt() == 1) EllipticalArcCurve.SweepFlag.CLOCKWISE else EllipticalArcCurve.SweepFlag.ANTICLOCKWISE
-        val end = Point(components[5].toFloat(), components[6].toFloat())
+    private fun mapEllipticalArcCurveParameter(components: List<Float>): EllipticalArcCurve.Parameter {
+        val radiusX = components[0]
+        val radiusY = components[1]
+        val angle = components[2]
+        val arcFlag = when (components[3]) {
+            1f -> EllipticalArcCurve.ArcFlag.LARGE
+            0f -> EllipticalArcCurve.ArcFlag.SMALL
+            else -> throw IllegalArgumentException("Unexpected elliptical curve arc flag value: ${components[4]}\nExpected 0 or 1.")
+        }
+        val sweepFlag = when(components[4]) {
+            1f -> EllipticalArcCurve.SweepFlag.CLOCKWISE
+            0f -> EllipticalArcCurve.SweepFlag.ANTICLOCKWISE
+            else -> throw IllegalArgumentException("Unexpected elliptical curve sweep flag value: ${components[4]}\nExpected 0 or 1.")
+        }
+        val end = Point(components[5], components[6])
 
         return EllipticalArcCurve.Parameter(radiusX, radiusY, angle, arcFlag, sweepFlag, end)
     }
 
     companion object {
         private val commandRegex = Regex("(?=[MmLlHhVvCcSsQqTtAaZz])\\s*")
-        private val number = Regex("[-+]?(?:\\d*\\.\\d+|\\d+\\.?)([eE][-+]?\\d+)?")
+        private val number = Regex("[-+]?(?:\\d*\\.\\d+|\\d+\\.?)(?:[eE][-+]?\\d+)?")
         private val separator = Regex("[,\\s]")
-        private val points = Regex(Array(2) { number.pattern }.joinToString(separator = separator.pattern))
-        private val quadraticBezierCurveParameters = Regex(Array(2) { points.pattern }.joinToString(separator = separator.pattern))
-        private val cubicBezierCurveParameters = Regex(Array(3) { points.pattern }.joinToString(separator = separator.pattern))
-        private val shortcutCubicBezierCurveParameters = Regex(Array(2) { points.pattern }.joinToString(separator = separator.pattern))
-        private val arcCurveParameters = Regex(Array(7) { number.pattern }.joinToString(separator = separator.pattern))
+        private val points = Regex("${number.pattern}${separator.pattern}${number.pattern}")
     }
 }
