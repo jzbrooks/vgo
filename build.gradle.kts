f788fed0bc6da6cdd7acaf0d0790cf390b2ea68e
Justin Brooks <justinbrooks@fastmail.com>
2020-03-15 16:42:07 -0500

    Add stdout test result summaries

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,23 @@ allprojects {
     }
 
     tasks.withType<Test> {
-        addTestOutputListener { desc, result -> 
-            println("${desc.name}: ${result.message}")
-        }
+        addTestListener(object : TestListener {
+            override fun afterSuite(suite: TestDescriptor, result: TestResult) {
+                if (suite.parent == null) {
+                    val output = "|  Results: ${result.resultType} (${result.testCount} tests, ${result.successfulTestCount} passed, ${result.failedTestCount} failed, ${result.skippedTestCount} skipped)  |"
+                    val border = "-".repeat(output.length)
+                    println("""
+                        $border
+                        $output
+                        $border
+                    """.trimIndent()
+                    )
+                }
+            }
+
+            override fun afterTest(testDescriptor: TestDescriptor?, result: TestResult?) {}
+            override fun beforeTest(testDescriptor: TestDescriptor?) {}
+            override fun beforeSuite(suite: TestDescriptor?) {}
+        })
     }
 }
