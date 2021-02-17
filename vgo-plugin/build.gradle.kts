2719f4110158f0d2c462a776e848877d5b7772ae
Justin Brooks <justinbrooks@fastmail.com>
2021-02-16 21:29:21 -0600

    Update assertk

--- vgo-plugin/build.gradle.kts
+++ vgo-plugin/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 dependencies {
     implementation(project(":vgo"))
 
-    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.22")
+    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.23.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.6.0")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.6.0")
--- vgo/build.gradle.kts
+++ vgo/build.gradle.kts
@@ -20,7 +20,7 @@ sourceSets {
 dependencies {
     implementation(project(":vgo-core"))
 
-    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.22")
+    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.23.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.6.0")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.6.0")
