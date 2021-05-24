a0658453c2f4d6954bb08df19f280dc1ef9b0e1b
Justin Brooks <justin@jzbrooks.com>
2021-05-23 22:54:15 -0500

    More simplification

--- vgo-core/src/main/kotlin/com/jzbrooks/vgo/core/optimization/BakeTransformations.kt
+++ vgo-core/src/main/kotlin/com/jzbrooks/vgo/core/optimization/BakeTransformations.kt
@@ -18,46 +18,13 @@ class BakeTransformations(private val transformKeys: HashSet<String>) : GroupVis
     private fun bakeIntoGroup(group: Group) {
         val groupTransform = group.transform
 
-        val children = mutableListOf<Element>()
+        if (group.elements.any { it !is PathElement } || groupTransform == Matrix3.IDENTITY) return
+
         for (child in group.elements) {
-            if (child is Group) {
-                val childTransform = child.transform
-                val childForeignTransformations = child.foreign.filterKeys(transformKeys::contains)
-
-                if (childForeignTransformations.isEmpty()) {
-                    child.transform = groupTransform * childTransform
-                } else {
-                    // If the child has a foreign transform value (usually this means non-literal),
-                    // then merging isn't possible. Wrap the child in a new group with the
-                    // current group transform value and proceed to bake siblings in
-                    // case this child had other path element siblings.
-
-                    for ((transform) in childForeignTransformations) {
-                        child.foreign.remove(transform)
-                    }
-
-                    val syntheticGroup = Group(
-                        listOf(child),
-                        null,
-                        childForeignTransformations.toMutableMap(),
-                        groupTransform * childTransform,
-                    )
-
-                    children.add(syntheticGroup)
-                }
-            } else if (child is PathElement) {
-
-                if (groupTransform !== Matrix3.IDENTITY) {
-                    child.applyTransform(groupTransform)
-                }
-
-                children.add(child)
-            }
+            (child as PathElement).applyTransform(groupTransform)
         }
 
         // Transform is baked. We don't want to apply it twice.
         group.transform = Matrix3.IDENTITY
-
-        group.elements = children
     }
 }
