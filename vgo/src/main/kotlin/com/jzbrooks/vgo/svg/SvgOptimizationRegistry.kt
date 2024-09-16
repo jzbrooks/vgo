46937c9c5dcb36222f6c3b461383d10d78b8f485
Justin Brooks <justin@jzbrooks.com>
2024-09-16 12:45:07 -0400

    Reenable convert curves to arc for svg

--- vgo/src/main/kotlin/com/jzbrooks/vgo/svg/SvgOptimizationRegistry.kt
+++ vgo/src/main/kotlin/com/jzbrooks/vgo/svg/SvgOptimizationRegistry.kt
@@ -4,6 +4,7 @@ import com.jzbrooks.vgo.core.optimization.BakeTransformations
 import com.jzbrooks.vgo.core.optimization.BreakoutImplicitCommands
 import com.jzbrooks.vgo.core.optimization.CollapseGroups
 import com.jzbrooks.vgo.core.optimization.CommandVariant
+import com.jzbrooks.vgo.core.optimization.ConvertCurvesToArcs
 import com.jzbrooks.vgo.core.optimization.MergePaths
 import com.jzbrooks.vgo.core.optimization.OptimizationRegistry
 import com.jzbrooks.vgo.core.optimization.Polycommands
@@ -29,7 +30,7 @@ class SvgOptimizationRegistry : OptimizationRegistry(BOTTOM_UP, TOP_DOWN) {
                 BreakoutImplicitCommands(),
                 CommandVariant(CommandVariant.Mode.Relative),
                 SimplifyLineCommands(1e-3f),
-//                ConvertCurvesToArcs(ScalableVectorGraphicCommandPrinter(3)),
+                ConvertCurvesToArcs(ScalableVectorGraphicCommandPrinter(3)),
                 SimplifyBezierCurveCommands(1e-3f),
                 RemoveRedundantCommands(),
                 CommandVariant(CommandVariant.Mode.Compact(ScalableVectorGraphicCommandPrinter(3))),
