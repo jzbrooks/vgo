079eed7ef9525be11391fea7745cd079f28a7426
Justin Brooks <justin@jzbrooks.com>
2025-01-19 20:34:40 -0500

    Add comments

--- vgo-core/src/main/kotlin/com/jzbrooks/vgo/core/transformation/BottomUpTransformation.kt
+++ vgo-core/src/main/kotlin/com/jzbrooks/vgo/core/transformation/BottomUpTransformation.kt
@@ -2,4 +2,5 @@ package com.jzbrooks.vgo.core.transformation
 
 import com.jzbrooks.vgo.core.graphic.ElementVisitor
 
+/** This transformation should be applied from the leaf [com.jzbrooks.vgo.core.graphic.Element] to the root */
 interface BottomUpTransformation : ElementVisitor
