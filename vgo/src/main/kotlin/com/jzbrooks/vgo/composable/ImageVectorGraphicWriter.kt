e3f9b9ee6196c2808e09ec4a5b212e56518ff37f
Justin Brooks <justin@jzbrooks.com>
2025-03-15 14:13:32 -0400

    Avoid clients needing to re-parse path data string

--- vgo/src/main/kotlin/com/jzbrooks/vgo/composable/ImageVectorGraphicWriter.kt
+++ vgo/src/main/kotlin/com/jzbrooks/vgo/composable/ImageVectorGraphicWriter.kt
@@ -2,7 +2,17 @@ package com.jzbrooks.vgo.composable
 
 import com.jzbrooks.vgo.core.Writer
 import com.jzbrooks.vgo.core.graphic.Path
-import com.jzbrooks.vgo.vd.VectorDrawableCommandPrinter
+import com.jzbrooks.vgo.core.graphic.command.ClosePath
+import com.jzbrooks.vgo.core.graphic.command.CommandVariant
+import com.jzbrooks.vgo.core.graphic.command.CubicBezierCurve
+import com.jzbrooks.vgo.core.graphic.command.EllipticalArcCurve
+import com.jzbrooks.vgo.core.graphic.command.HorizontalLineTo
+import com.jzbrooks.vgo.core.graphic.command.LineTo
+import com.jzbrooks.vgo.core.graphic.command.MoveTo
+import com.jzbrooks.vgo.core.graphic.command.QuadraticBezierCurve
+import com.jzbrooks.vgo.core.graphic.command.SmoothCubicBezierCurve
+import com.jzbrooks.vgo.core.graphic.command.SmoothQuadraticBezierCurve
+import com.jzbrooks.vgo.core.graphic.command.VerticalLineTo
 import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FileSpec
@@ -34,54 +44,224 @@ class ImageVectorGraphicWriter(
 
     private fun createImageVectorProperty(graphic: ImageVectorGraphic): PropertySpec {
         val imageVector = ClassName("androidx.compose.ui.graphics.vector", "ImageVector")
-        val vectorBuilder = ClassName("androidx.compose.ui.graphics.vector", "ImageVector.Builder")
         val composeColor = MemberName("androidx.compose.ui.graphics", "Color")
         val solidColorBrush = ClassName("androidx.compose.ui.graphics", "SolidColor")
-        val addPathNodes = MemberName("androidx.compose.ui.graphics.vector", "addPathNodes")
 
         val codeBlock =
             CodeBlock
                 .builder()
                 .add(
-                    "%T(defaultWidth = %L, defaultHeight = %L, viewportWidth = %L, viewportHeight = %L)\n",
-                    vectorBuilder,
+                    "%T.Builder(defaultWidth = %L, defaultHeight = %L, viewportWidth = %L, viewportHeight = %L)\n",
+                    imageVector,
                     graphic.foreign.getValue("defaultWidth") + ".dp",
                     graphic.foreign.getValue("defaultHeight") + ".dp",
                     graphic.foreign.getValue("viewportWidth"),
                     graphic.foreign.getValue("viewportHeight"),
                 ).indent()
 
-        codeBlock.add(".apply {\n")
         codeBlock.withIndent {
-            graphic.elements.filterIsInstance<Path>().forEach { path ->
-                codeBlock.add("addPath(\n")
-                codeBlock.withIndent {
-                    codeBlock.add("pathData = %M(%S),\n", addPathNodes, convertPathCommandsToString(path))
-                    codeBlock.add(
-                        "fill = %T(%M(%L, %L, %L, %L)),\n",
-                        solidColorBrush,
-                        composeColor,
-                        path.fill.red,
-                        path.fill.green,
-                        path.fill.blue,
-                        path.fill.alpha,
-                    )
-                    codeBlock.add(
-                        "stroke = %T(%M(%L, %L, %L, %L)),\n",
-                        solidColorBrush,
-                        composeColor,
-                        path.stroke.red,
-                        path.stroke.green,
-                        path.stroke.blue,
-                        path.stroke.alpha,
-                    )
-                    codeBlock.add("strokeLineWidth = %Lf\n", path.strokeWidth)
+            for (element in graphic.elements) {
+                when (element) {
+                    is Path -> {
+                        codeBlock.add(".path(\n")
+                        codeBlock.withIndent {
+                            if (element.fill.alpha > 0u) {
+                                codeBlock.add(
+                                    "fill = %T(%M(%L, %L, %L, %L)),\n",
+                                    solidColorBrush,
+                                    composeColor,
+                                    element.fill.red,
+                                    element.fill.green,
+                                    element.fill.blue,
+                                    element.fill.alpha,
+                                )
+                            }
+
+                            if (element.stroke.alpha > 0u && element.strokeWidth > 0f) {
+                                codeBlock.add(
+                                    "stroke = %T(%M(%L, %L, %L, %L)),\n",
+                                    solidColorBrush,
+                                    composeColor,
+                                    element.stroke.red,
+                                    element.stroke.green,
+                                    element.stroke.blue,
+                                    element.stroke.alpha,
+                                )
+                                codeBlock.add("strokeLineWidth = %Lf\n", element.strokeWidth)
+                            }
+                        }
+                        codeBlock.add(") {\n")
+                        codeBlock.withIndent {
+                            for (command in element.commands) {
+                                when (command) {
+                                    is MoveTo -> {
+                                        val coord = command.parameters.first()
+                                        if (command.variant == CommandVariant.ABSOLUTE) {
+                                            codeBlock.add("moveTo(%Lf, %Lf)\n", coord.x, coord.y)
+                                        } else {
+                                            codeBlock.add("moveToRelative(%Lf, %Lf)\n", coord.x, coord.y)
+                                        }
+
+                                        for (parameter in command.parameters.drop(1)) {
+                                            if (command.variant == CommandVariant.ABSOLUTE) {
+                                                codeBlock.add("lineTo(%Lf, %Lf)\n", parameter.x, parameter.y)
+                                            } else {
+                                                codeBlock.add("lineToRelative(%Lf, %Lf)\n", parameter.x, parameter.y)
+                                            }
+                                        }
+                                    }
+                                    is LineTo -> {
+                                        for (parameter in command.parameters) {
+                                            if (command.variant == CommandVariant.ABSOLUTE) {
+                                                codeBlock.add("lineTo(%Lf, %Lf)\n", parameter.x, parameter.y)
+                                            } else {
+                                                codeBlock.add("lineToRelative(%Lf, %Lf)\n", parameter.x, parameter.y)
+                                            }
+                                        }
+                                    }
+                                    is HorizontalLineTo -> {
+                                        for (parameter in command.parameters) {
+                                            if (command.variant == CommandVariant.ABSOLUTE) {
+                                                codeBlock.add("horizontalLineTo(%Lf)\n", parameter)
+                                            } else {
+                                                codeBlock.add("horizontalLineToRelative(%Lf)\n", parameter)
+                                            }
+                                        }
+                                    }
+                                    is VerticalLineTo -> {
+                                        for (parameter in command.parameters) {
+                                            if (command.variant == CommandVariant.ABSOLUTE) {
+                                                codeBlock.add("verticalLineTo(%Lf)\n", parameter)
+                                            } else {
+                                                codeBlock.add("verticalLineToRelative(%Lf)\n", parameter)
+                                            }
+                                        }
+                                    }
+                                    is CubicBezierCurve -> {
+                                        for (parameter in command.parameters) {
+                                            if (command.variant == CommandVariant.ABSOLUTE) {
+                                                codeBlock.add(
+                                                    "curveTo(%Lf, %Lf, %Lf, %Lf, %Lf, %Lf)\n",
+                                                    parameter.startControl.x,
+                                                    parameter.startControl.y,
+                                                    parameter.endControl.x,
+                                                    parameter.endControl.y,
+                                                    parameter.end.x,
+                                                    parameter.end.y,
+                                                )
+                                            } else {
+                                                codeBlock.add(
+                                                    "curveToRelative(%Lf, %Lf, %Lf, %Lf, %Lf, %Lf)\n",
+                                                    parameter.startControl.x,
+                                                    parameter.startControl.y,
+                                                    parameter.endControl.x,
+                                                    parameter.endControl.y,
+                                                    parameter.end.x,
+                                                    parameter.end.y,
+                                                )
+                                            }
+                                        }
+                                    }
+                                    is SmoothCubicBezierCurve -> {
+                                        for (parameter in command.parameters) {
+                                            if (command.variant == CommandVariant.ABSOLUTE) {
+                                                codeBlock.add(
+                                                    "reflectiveCurveTo(%Lf, %Lf, %Lf, %Lf)\n",
+                                                    parameter.endControl.x,
+                                                    parameter.endControl.y,
+                                                    parameter.end.x,
+                                                    parameter.end.y,
+                                                )
+                                            } else {
+                                                codeBlock.add(
+                                                    "reflectiveCurveToRelative(%Lf, %Lf, %Lf, %Lf)\n",
+                                                    parameter.endControl.x,
+                                                    parameter.endControl.y,
+                                                    parameter.end.x,
+                                                    parameter.end.y,
+                                                )
+                                            }
+                                        }
+                                    }
+                                    is QuadraticBezierCurve -> {
+                                        for (parameter in command.parameters) {
+                                            if (command.variant == CommandVariant.ABSOLUTE) {
+                                                codeBlock.add(
+                                                    "quadTo(%Lf, %Lf, %Lf, %Lf)\n",
+                                                    parameter.control.x,
+                                                    parameter.control.y,
+                                                    parameter.end.x,
+                                                    parameter.end.y,
+                                                )
+                                            } else {
+                                                codeBlock.add(
+                                                    "quadToRelative(%Lf, %Lf, %Lf, %Lf)\n",
+                                                    parameter.control.x,
+                                                    parameter.control.y,
+                                                    parameter.end.x,
+                                                    parameter.end.y,
+                                                )
+                                            }
+                                        }
+                                    }
+                                    is SmoothQuadraticBezierCurve -> {
+                                        for (parameter in command.parameters) {
+                                            if (command.variant == CommandVariant.ABSOLUTE) {
+                                                codeBlock.add(
+                                                    "reflectiveQuadTo(%Lf, %Lf)\n",
+                                                    parameter.x,
+                                                    parameter.y,
+                                                )
+                                            } else {
+                                                codeBlock.add(
+                                                    "reflectiveQuadToRelative(%Lf, %Lf)\n",
+                                                    parameter.x,
+                                                    parameter.y,
+                                                )
+                                            }
+                                        }
+                                    }
+
+                                    is EllipticalArcCurve -> {
+                                        for (parameter in command.parameters) {
+                                            if (command.variant == CommandVariant.ABSOLUTE) {
+                                                codeBlock.add(
+                                                    "arcTo(%Lf, %Lf, %Lf, %L, %L, %Lf, %Lf)\n",
+                                                    parameter.radiusX,
+                                                    parameter.radiusY,
+                                                    parameter.angle,
+                                                    parameter.arc == EllipticalArcCurve.ArcFlag.LARGE,
+                                                    parameter.sweep == EllipticalArcCurve.SweepFlag.CLOCKWISE,
+                                                    parameter.end.x,
+                                                    parameter.end.y,
+                                                )
+                                            } else {
+                                                codeBlock.add(
+                                                    "arcToRelative(%Lf, %Lf, %Lf, %L, %L, %Lf, %Lf)\n",
+                                                    parameter.radiusX,
+                                                    parameter.radiusY,
+                                                    parameter.angle,
+                                                    parameter.arc == EllipticalArcCurve.ArcFlag.LARGE,
+                                                    parameter.sweep == EllipticalArcCurve.SweepFlag.CLOCKWISE,
+                                                    parameter.end.x,
+                                                    parameter.end.y,
+                                                )
+                                            }
+                                        }
+                                    }
+
+                                    ClosePath -> {
+                                        codeBlock.add("close()\n")
+                                    }
+                                }
+                            }
+                        }
+                        codeBlock.add("}\n")
+                    }
                 }
-                codeBlock.add(")\n")
             }
         }
 
-        codeBlock.add("}\n")
         codeBlock.add(".build()")
         codeBlock.unindent()
 
@@ -89,10 +269,5 @@ class ImageVectorGraphicWriter(
             .builder(graphic.id.toString(), imageVector)
             .initializer(codeBlock.build())
             .build()
-    }
-
-    private fun convertPathCommandsToString(path: Path): String {
-        val printer = VectorDrawableCommandPrinter(3)
-        return path.commands.joinToString("", transform = printer::print)
     }
 }
