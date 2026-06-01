b82f8d0234857f844af08bd6c7a5877fce3e250a
Justin Brooks <justin@jzbrooks.com>
2026-05-31 21:26:18 -0400

    De-optimize baselines

--- vgo/src/test/resources/guacamole.kt
+++ vgo/src/test/resources/guacamole.kt
@@ -11,113 +11,115 @@ public val vector: ImageVector
     .group(
       rotate = 0f,
       scaleX = 1f,
-      scaleY = 1f,
-      translationX = 0f,
-      translationY = 0f,
+      scaleY = 1.22f,
+      translationX = -1.31f,
+      translationY = -65.52f,
     ) {
       group(
         clipPathData = listOf(
-          PathNode.MoveTo(858.3f, 326.1f),
-          PathNode.RelativeLineTo(-565.4f, -67.2f),
-          PathNode.LineTo(283f, 383f),
-          PathNode.RelativeLineTo(565.4f, 67.3f),
-          PathNode.RelativeLineTo(9.9f, -124.2f),
+          PathNode.MoveTo(859.61f, 320.594f),
+          PathNode.LineTo(294.25f, 265.52f),
+          PathNode.LineTo(284.35f, 367.15f),
+          PathNode.LineTo(849.71f, 422.22f),
+          PathNode.LineTo(859.61f, 320.59f),
+          PathNode.Close,
         ),
       ) {
-        group(
-          rotate = 0f,
-          scaleX = 1f,
-          scaleY = 1f,
-          translationX = 0f,
-          translationY = 0f,
+        path(
+          fill = SolidColor(Color(63, 178, 73, 255)),
         ) {
-          path(
-            fill = SolidColor(Color(63, 178, 73, 255)),
-          ) {
-            moveTo(443.7f, 298.5f)
-            curveToRelative(5.1f, -1.1f, 9.8f, -3.5f, 13.7f, -6.7f)
-            curveToRelative(5.7f, 8.7f, 15.6f, 14.4f, 26.8f, 14.4f)
-            curveToRelative(9.1f, 0f, 17.2f, -3.7f, 23.1f, -9.7f)
-            curveToRelative(6.3f, 2.5f, 13.5f, 3.9f, 21.2f, 3.9f)
-            curveToRelative(9.2f, 0f, 17.8f, -2.1f, 24.8f, -5.5f)
-            curveToRelative(4f, 13f, 16.2f, 22.4f, 30.6f, 22.4f)
-            curveToRelative(5.1f, 0f, 9.8f, -1.1f, 14f, -3.1f)
-            curveToRelative(5.6f, 7.5f, 16f, 12.5f, 28f, 12.5f)
-            curveToRelative(6.2f, 0f, 12f, -1.3f, 16.9f, -3.7f)
-            curveToRelative(5.9f, 7.4f, 15f, 12.2f, 25.2f, 12.2f)
-            curveToRelative(8.1f, 0f, 15.4f, -2.9f, 21f, -7.8f)
-            curveToRelative(5.9f, 5.4f, 14.7f, 8.8f, 24.5f, 8.8f)
-            curveToRelative(8.3f, 0f, 15.9f, -2.4f, 21.6f, -6.5f)
-            curveToRelative(5.8f, 8.2f, 15.5f, 13.6f, 26.3f, 13.6f)
-            curveToRelative(6.5f, 0f, 12.4f, -1.8f, 17.4f, -5f)
-            curveToRelative(4.3f, 3.2f, 9.4f, 5.2f, 14.8f, 5.8f)
-            curveToRelative(4.9f, 5.6f, 12.6f, 9.5f, 21.4f, 10.6f)
-            curveToRelative(5.9f, 7.1f, 14.8f, 11.6f, 24.8f, 11.6f)
-            lineToRelative(1.7f, 0f)
-            curveToRelative(-3.1f, 3.8f, -4.8f, 8.3f, -4.8f, 13.2f)
-            curveToRelative(0f, 8.6f, 5.5f, 16.2f, 14f, 20.7f)
-            curveTo(828f, 489.6f, 702.3f, 546.4f, 556.8f, 529.1f)
-            curveToRelative(-139.3f, -16.5f, -249.9f, -95.4f, -266.4f, -183.7f)
-            curveToRelative(7.8f, -4.6f, 13f, -11.9f, 13f, -20.2f)
-            curveToRelative(0f, -.7f, -.1f, -1.3f, -.2f, -2f)
-            curveToRelative(2.1f, .3f, 4.2f, .5f, 6.4f, .5f)
-            curveToRelative(12.1f, 0f, 22.6f, -5.2f, 28.1f, -12.9f)
-            curveToRelative(2.8f, .7f, 5.7f, 1.1f, 8.7f, 1.1f)
-            curveToRelative(3.6f, 0f, 7.1f, -.6f, 10.4f, -1.7f)
-            curveToRelative(5.9f, 6.4f, 14.3f, 10.4f, 23.7f, 10.4f)
-            curveToRelative(9.6f, 0f, 18.1f, -4.1f, 24f, -10.7f)
-            curveToRelative(3.4f, 1f, 7.1f, 1.5f, 11f, 1.5f)
-            curveToRelative(12.1f, 0f, 22.7f, -5.2f, 28.2f, -12.9f)
-            close()
-            moveToRelative(391.6f, 4.5f)
-            lineToRelative(.2f, .3f)
-            curveToRelative(-.2f, 0f, -.4f, 0f, -.6f, .1f)
-            curveToRelative(.1f, -.2f, .1f, -.5f, .2f, -.7f)
-            lineToRelative(.2f, .3f)
-            close()
-            moveToRelative(-511.7f, -56.5f)
-            curveToRelative(-2f, -1.1f, -4.1f, -2f, -6.4f, -2.6f)
-            curveToRelative(48.5f, -59.7f, 152.2f, -93.3f, 268.6f, -79.4f)
-            curveToRelative(89.9f, 10.6f, 167.9f, 47.3f, 216.3f, 95.7f)
-            curveToRelative(-13.2f, 1.2f, -24.1f, 10.4f, -27.7f, 22.6f)
-            curveToRelative(-4f, -1.8f, -8.3f, -2.7f, -13f, -2.7f)
-            curveToRelative(-4.6f, 0f, -9.1f, .9f, -13.1f, 2.7f)
-            curveToRelative(0f, -17.4f, -14.3f, -31.6f, -32f, -31.6f)
-            curveToRelative(-15.5f, 0f, -28.5f, 10.9f, -31.4f, 25.5f)
-            curveToRelative(-4.9f, -3.1f, -10.7f, -4.8f, -16.9f, -4.8f)
-            curveToRelative(-1.1f, 0f, -2.2f, 0f, -3.3f, .2f)
-            lineToRelative(0f, -.4f)
-            curveToRelative(0f, -17.4f, -19.7f, -31.6f, -44f, -31.6f)
-            curveToRelative(-15.1f, 0f, -28.5f, 5.5f, -36.4f, 13.9f)
-            lineToRelative(-.4f, 0f)
-            curveToRelative(-5.1f, 0f, -10f, 1.2f, -14.4f, 3.4f)
-            curveToRelative(-6.3f, -11.9f, -22.3f, -20.3f, -41f, -20.3f)
-            curveToRelative(-11.9f, 0f, -22.6f, 3.4f, -30.5f, 8.9f)
-            curveToRelative(-4.2f, -2f, -8.9f, -3.1f, -13.8f, -3.1f)
-            curveToRelative(-7.8f, 0f, -15f, 2.8f, -20.5f, 7.4f)
-            curveToRelative(-5.7f, -8.7f, -15.6f, -14.4f, -26.8f, -14.4f)
-            curveToRelative(-6.4f, 0f, -12.4f, 1.9f, -17.4f, 5.1f)
-            curveToRelative(-6.3f, -11.9f, -22.3f, -20.4f, -41.1f, -20.4f)
-            curveToRelative(-17.5f, 0f, -32.6f, 7.4f, -39.7f, 18.1f)
-            curveToRelative(-5.6f, 1.3f, -10.8f, 4f, -15.1f, 7.8f)
-            close()
-          }
+          moveTo(445.03f, 297.939f)
+          curveTo(450.14f, 297.04f, 454.8f, 295.14f, 458.68f, 292.52f)
+          curveTo(464.4f, 299.6f, 474.27f, 304.28f, 485.49f, 304.28f)
+          curveTo(494.56f, 304.28f, 502.76f, 301.22f, 508.58f, 296.3f)
+          curveTo(514.88f, 298.35f, 522.11f, 299.52f, 529.8f, 299.52f)
+          curveTo(539.01f, 299.52f, 547.56f, 297.85f, 554.63f, 294.99f)
+          curveTo(558.61f, 305.63f, 570.83f, 313.38f, 585.26f, 313.38f)
+          curveTo(590.26f, 313.38f, 595f, 312.45f, 599.22f, 310.79f)
+          curveTo(604.81f, 316.94f, 615.24f, 321.08f, 627.17f, 321.08f)
+          curveTo(633.38f, 321.08f, 639.18f, 319.96f, 644.11f, 318.01f)
+          curveTo(649.97f, 324.09f, 659.1f, 328f, 669.35f, 328f)
+          curveTo(677.37f, 328f, 684.7f, 325.61f, 690.32f, 321.66f)
+          curveTo(696.24f, 326.06f, 705.01f, 328.85f, 714.8f, 328.85f)
+          curveTo(723.13f, 328.85f, 730.72f, 326.83f, 736.45f, 323.52f)
+          curveTo(742.23f, 330.26f, 751.86f, 334.68f, 762.76f, 334.68f)
+          curveTo(769.16f, 334.68f, 775.12f, 333.16f, 780.13f, 330.53f)
+          curveTo(784.44f, 333.14f, 789.49f, 334.84f, 794.94f, 335.32f)
+          curveTo(799.83f, 339.85f, 807.48f, 343.07f, 816.33f, 343.99f)
+          curveTo(822.2f, 349.77f, 831.11f, 353.47f, 841.07f, 353.47f)
+          curveTo(841.64f, 353.47f, 842.21f, 353.46f, 842.77f, 353.43f)
+          curveTo(839.73f, 356.58f, 837.97f, 360.28f, 837.97f, 364.24f)
+          curveTo(837.97f, 371.29f, 843.53f, 377.52f, 852.01f, 381.24f)
+          curveTo(829.27f, 454.42f, 703.6f, 500.93f, 558.06f, 486.75f)
+          curveTo(418.85f, 473.19f, 308.19f, 408.66f, 291.67f, 336.39f)
+          curveTo(299.56f, 332.64f, 304.67f, 326.63f, 304.67f, 319.87f)
+          curveTo(304.67f, 319.3f, 304.63f, 318.73f, 304.56f, 318.17f)
+          curveTo(306.61f, 318.43f, 308.73f, 318.56f, 310.89f, 318.56f)
+          curveTo(322.98f, 318.56f, 333.53f, 314.31f, 339.06f, 308.02f)
+          curveTo(341.82f, 308.61f, 344.71f, 308.92f, 347.68f, 308.92f)
+          curveTo(351.32f, 308.92f, 354.83f, 308.45f, 358.14f, 307.58f)
+          curveTo(364f, 312.8f, 372.45f, 316.09f, 381.84f, 316.09f)
+          curveTo(391.38f, 316.09f, 399.95f, 312.7f, 405.82f, 307.34f)
+          curveTo(409.24f, 308.13f, 412.94f, 308.56f, 416.79f, 308.56f)
+          curveTo(428.93f, 308.56f, 439.52f, 304.27f, 445.03f, 297.94f)
+          close()
+          moveTo(836.59f, 301.647f)
+          lineTo(836.76f, 301.89f)
+          curveTo(836.57f, 301.92f, 836.37f, 301.94f, 836.18f, 301.96f)
+          curveTo(836.27f, 301.78f, 836.35f, 301.6f, 836.43f, 301.42f)
+          lineTo(836.59f, 301.65f)
+          close()
+          moveTo(324.9f, 255.368f)
+          curveTo(322.9f, 254.5f, 320.78f, 253.8f, 318.55f, 253.29f)
+          curveTo(367.04f, 204.39f, 470.73f, 176.91f, 587.14f, 188.24f)
+          curveTo(677f, 197f, 754.96f, 226.99f, 803.39f, 266.59f)
+          curveTo(790.2f, 267.6f, 779.34f, 275.1f, 775.72f, 285.09f)
+          curveTo(771.76f, 283.67f, 767.37f, 282.88f, 762.76f, 282.88f)
+          curveTo(758.09f, 282.88f, 753.65f, 283.69f, 749.65f, 285.14f)
+          lineTo(749.65f, 285.14f)
+          curveTo(749.65f, 270.84f, 735.31f, 259.24f, 717.65f, 259.24f)
+          curveTo(702.11f, 259.24f, 689.15f, 268.21f, 686.25f, 280.1f)
+          curveTo(681.34f, 277.62f, 675.55f, 276.19f, 669.35f, 276.19f)
+          curveTo(668.21f, 276.19f, 667.09f, 276.24f, 665.98f, 276.33f)
+          curveTo(665.98f, 276.25f, 665.98f, 276.16f, 665.98f, 276.07f)
+          curveTo(665.98f, 261.77f, 646.28f, 250.17f, 622.02f, 250.17f)
+          curveTo(606.87f, 250.17f, 593.5f, 254.69f, 585.59f, 261.57f)
+          curveTo(585.48f, 261.57f, 585.37f, 261.57f, 585.26f, 261.57f)
+          curveTo(580.08f, 261.57f, 575.19f, 262.57f, 570.86f, 264.34f)
+          curveTo(564.52f, 254.62f, 548.52f, 247.71f, 529.8f, 247.71f)
+          curveTo(517.94f, 247.71f, 507.18f, 250.49f, 499.26f, 254.99f)
+          curveTo(495.09f, 253.38f, 490.42f, 252.47f, 485.49f, 252.47f)
+          curveTo(477.7f, 252.47f, 470.55f, 254.73f, 464.99f, 258.49f)
+          curveTo(459.28f, 251.41f, 449.4f, 246.73f, 438.18f, 246.73f)
+          curveTo(431.78f, 246.73f, 425.82f, 248.25f, 420.81f, 250.88f)
+          curveTo(414.48f, 241.15f, 398.48f, 234.25f, 379.76f, 234.25f)
+          curveTo(362.25f, 234.25f, 347.13f, 240.29f, 340.05f, 249.02f)
+          curveTo(334.38f, 250.08f, 329.2f, 252.3f, 324.9f, 255.37f)
+          close()
         }
       }
     }
-    .path(
-      fill = SolidColor(Color(29, 29, 29, 255)),
+    .group(
+      rotate = 0f,
+      scaleX = 1f,
+      scaleY = 1f,
+      translationX = 90.91f,
+      translationY = -157.9f,
     ) {
-      moveTo(253.2f, 327.3f)
-      curveToRelative(-1.1f, -3.8f, .1f, -7.8f, 3.2f, -10.3f)
-      curveToRelative(3f, -2.6f, 7.2f, -3.1f, 10.7f, -1.3f)
-      curveToRelative(95.4f, 46.3f, 216.3f, 74f, 347.8f, 73.9f)
-      curveToRelative(95.1f, 0f, 184.7f, -14.6f, 263.1f, -40.3f)
-      curveToRelative(3.7f, -1.2f, 7.8f, -.2f, 10.5f, 2.7f)
-      curveToRelative(2.6f, 2.8f, 3.4f, 7f, 1.9f, 10.6f)
-      curveToRelative(-51.1f, 122f, -171.7f, 207.9f, -312.2f, 208f)
-      curveToRelative(-153.8f, .1f, -283.9f, -102.7f, -325f, -243.3f)
-      close()
+      path(
+        fill = SolidColor(Color(29, 29, 29, 255)),
+      ) {
+        moveTo(162.24f, 485.218f)
+        curveTo(161.17f, 481.43f, 162.43f, 477.37f, 165.46f, 474.86f)
+        curveTo(168.49f, 472.34f, 172.71f, 471.84f, 176.24f, 473.58f)
+        curveTo(271.58f, 519.89f, 392.49f, 547.58f, 523.96f, 547.51f)
+        curveTo(619.11f, 547.47f, 708.72f, 532.89f, 787.09f, 507.24f)
+        curveTo(790.79f, 506f, 794.87f, 507.02f, 797.55f, 509.87f)
+        curveTo(800.22f, 512.72f, 800.99f, 516.86f, 799.52f, 520.48f)
+        curveTo(748.41f, 642.55f, 627.81f, 728.44f, 487.28f, 728.51f)
+        curveTo(333.46f, 728.59f, 203.41f, 625.81f, 162.24f, 485.22f)
+        close()
+      }
     }
   .build().also { _vector = it }
 
--- vgo/src/test/resources/visibility_strike.kt
+++ vgo/src/test/resources/visibility_strike.kt
@@ -13,51 +13,49 @@ public val visibilitystrike: ImageVector
     ) {
       moveTo(2f, 4.27f)
       lineTo(3.27f, 3f)
+      lineTo(3.27f, 3f)
       lineTo(2f, 4.27f)
+      close()
     }
     .group(
       clipPathData = listOf(
         PathNode.MoveTo(0f, 0.0f),
-        PathNode.HorizontalLineTo(24f),
-        PathNode.VerticalLineTo(24f),
-        PathNode.HorizontalLineTo(0f),
-        PathNode.VerticalLineTo(0f),
+        PathNode.LineTo(24f, 0f),
+        PathNode.LineTo(24f, 24f),
+        PathNode.LineTo(0f, 24f),
+        PathNode.LineTo(0f, 0f),
         PathNode.Close,
         PathNode.MoveTo(4.54f, 1.73f),
+        PathNode.LineTo(3.27f, 3f),
         PathNode.LineTo(3.27f, 3f),
         PathNode.LineTo(4.54f, 1.73f),
         PathNode.Close,
       ),
     ) {
-      group(
-        rotate = 0f,
-        scaleX = 1f,
-        scaleY = 1f,
-        translationX = 0f,
-        translationY = 0f,
+      path(
+        fill = SolidColor(Color(0, 0, 0, 255)),
       ) {
-        path(
-          fill = SolidColor(Color(0, 0, 0, 255)),
-        ) {
-          moveTo(12f, 4.5f)
-          curveTo(7f, 4.5f, 2.73f, 7.61f, 1f, 12f)
-          curveTo(2.73f, 16.39f, 7f, 19.5f, 12f, 19.5f)
-          reflectiveCurveTo(21.27f, 16.39f, 23f, 12f)
-          curveTo(21.27f, 7.61f, 17f, 4.5f, 12f, 4.5f)
-          close()
-          moveTo(12f, 17.0f)
-          curveTo(9.24f, 17f, 7f, 14.76f, 7f, 12f)
-          reflectiveCurveTo(9.24f, 7f, 12f, 7f)
-          reflectiveCurveTo(17f, 9.24f, 17f, 12f)
-          reflectiveCurveTo(14.76f, 17f, 12f, 17f)
-          close()
-          moveTo(12f, 9.0f)
-          curveTo(10.34f, 9f, 9f, 10.34f, 9f, 12f)
-          reflectiveCurveTo(10.34f, 15f, 12f, 15f)
-          reflectiveCurveTo(15f, 13.66f, 15f, 12f)
-          reflectiveCurveTo(13.66f, 9f, 12f, 9f)
-          close()
-        }
+        moveTo(12f, 4.5f)
+        curveTo(7f, 4.5f, 2.73f, 7.61f, 1f, 12f)
+        curveTo(2.73f, 16.39f, 7f, 19.5f, 12f, 19.5f)
+        curveTo(17f, 19.5f, 21.27f, 16.39f, 23f, 12f)
+        curveTo(21.27f, 7.61f, 17f, 4.5f, 12f, 4.5f)
+        lineTo(12f, 4.5f)
+        close()
+        moveTo(12f, 17.0f)
+        curveTo(9.24f, 17f, 7f, 14.76f, 7f, 12f)
+        curveTo(7f, 9.24f, 9.24f, 7f, 12f, 7f)
+        curveTo(14.76f, 7f, 17f, 9.24f, 17f, 12f)
+        curveTo(17f, 14.76f, 14.76f, 17f, 12f, 17f)
+        lineTo(12f, 17f)
+        close()
+        moveTo(12f, 9.0f)
+        curveTo(10.34f, 9f, 9f, 10.34f, 9f, 12f)
+        curveTo(9f, 13.66f, 10.34f, 15f, 12f, 15f)
+        curveTo(13.66f, 15f, 15f, 13.66f, 15f, 12f)
+        curveTo(15f, 10.34f, 13.66f, 9f, 12f, 9f)
+        lineTo(12f, 9f)
+        close()
       }
     }
   .build().also { _visibilitystrike = it }
