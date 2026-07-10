3d325edaa1ba5cc486ee4816cf5e59cf61df163e
Justin Brooks <justin@jzbrooks.com>
2026-07-10 16:08:45 -0400

    Modernize build scripts (#273)
    * Bring the buildscripts into 2026
    
    * Fix configuration cache issue

--- vgo-cli/build.gradle.kts
+++ vgo-cli/build.gradle.kts
@@ -1,62 +1,68 @@
 plugins {
-    id("org.jetbrains.kotlin.jvm")
+    id("vgo.kotlin-conventions")
+}
+
+val r8: Configuration = configurations.create("r8")
+
+dependencies {
+    implementation(project(":vgo"))
+    implementation(project(":vgo-core"))
+
+    implementation(libs.android.sdk.common)
+    implementation(libs.kotlin.compiler.embeddable)
+
+    r8(libs.r8)
+
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.junit.jupiter.api)
+    testImplementation(libs.junit.jupiter.params)
+    testImplementation(libs.junit.platform.launcher)
+    testRuntimeOnly(libs.junit.jupiter.engine)
+
+    testImplementation(libs.assertk)
 }
 
 tasks {
     jar {
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 
-        dependsOn(configurations.runtimeClasspath)
         manifest {
             attributes["Main-Class"] = "com.jzbrooks.vgo.cli.CommandLineInterface"
             attributes["Bundle-Version"] = providers.gradleProperty("VERSION_NAME").get()
-
-            exclude(
-                "META-INF/*.SF",
-                "META-INF/*.DSA",
-                "META-INF/*.RSA",
-                "META-INF/*.EC",
-                "META-INF/*.SF.*",
-                "META-INF/*.DSA.*",
-                "META-INF/*.RSA.*",
-                "META-INF/*.EC.*",
-                "META-INF/BCKEY.DSA",
-                "META-INF/BC2048KE.DSA",
-                "META-INF/BCKEY.SF",
-                "META-INF/BC2048KE.SF",
-                "**/*.kotlin_metadata",
-                "**/*.kotlin_module",
-                "**/*.kotlin_builtins",
-                "**/module-info.class",
-                "META-INF/maven/**",
-                "META-INF/versions/**",
-                "META-INF/*.version",
-                "META-INF/LICENSE*",
-                "META-INF/LGPL2.1",
-                "META-INF/DEPENDENCIES",
-                "META-INF/AL2.0",
-                "**/NOTICE*",
-                "javax/activation/**",
-                "xsd/catalog.xml",
-            )
         }
 
-        val sourceClasses =
-            sourceSets.main
-                .get()
-                .output.classesDirs
-        inputs.files(sourceClasses)
+        exclude(
+            "META-INF/*.SF",
+            "META-INF/*.DSA",
+            "META-INF/*.RSA",
+            "META-INF/*.EC",
+            "META-INF/*.SF.*",
+            "META-INF/*.DSA.*",
+            "META-INF/*.RSA.*",
+            "META-INF/*.EC.*",
+            "META-INF/BCKEY.DSA",
+            "META-INF/BC2048KE.DSA",
+            "META-INF/BCKEY.SF",
+            "META-INF/BC2048KE.SF",
+            "**/*.kotlin_metadata",
+            "**/*.kotlin_module",
+            "**/*.kotlin_builtins",
+            "**/module-info.class",
+            "META-INF/maven/**",
+            "META-INF/versions/**",
+            "META-INF/*.version",
+            "META-INF/LICENSE*",
+            "META-INF/LGPL2.1",
+            "META-INF/DEPENDENCIES",
+            "META-INF/AL2.0",
+            "**/NOTICE*",
+            "javax/activation/**",
+            "xsd/catalog.xml",
+        )
+
+        from(configurations.runtimeClasspath.map { classpath -> classpath.files.map(::zipTree) })
+
         destinationDirectory.set(layout.buildDirectory.dir("libs/debug"))
-
-        doFirst {
-            from(files(sourceClasses))
-            from(
-                configurations.runtimeClasspath
-                    .get()
-                    .asFileTree.files
-                    .map(::zipTree),
-            )
-        }
     }
 
     val optimize =
@@ -84,10 +90,9 @@ tasks {
                 layout.buildDirectory.file("libs/debug/vgo-cli.jar").get(),
             )
 
-            dependsOn(getByName("jar"))
+            dependsOn(jar)
         }
 
-    val binaryFileProp = layout.buildDirectory.file("libs/vgo")
     register("binary") {
         description = "Prepends shell script in the jar to improve CLI"
         group = "build"
@@ -95,37 +100,18 @@ tasks {
         dependsOn(optimize)
 
         inputs.file(layout.buildDirectory.file("libs/vgo.jar"))
-        outputs.file(binaryFileProp)
+        outputs.file(layout.buildDirectory.file("libs/vgo"))
 
         doLast {
-            val binaryFile = binaryFileProp.get().asFile
+            val binaryFile = outputs.files.singleFile
+            val optimizedJar = inputs.files.singleFile
             binaryFile.parentFile.mkdirs()
             binaryFile.delete()
             binaryFile.appendText("#!/bin/sh\n\nexec java \$JAVA_OPTS -jar \$0 \"\$@\"\n\n")
-            layout.buildDirectory.file("libs/vgo.jar").get().asFile.inputStream().use {
+            optimizedJar.inputStream().use {
                 binaryFile.appendBytes(it.readBytes())
             }
             binaryFile.setExecutable(true, false)
         }
     }
-}
-
-val r8: Configuration = configurations.create("r8")
-
-dependencies {
-    implementation(project(":vgo"))
-    implementation(project(":vgo-core"))
-
-    implementation("com.android.tools:sdk-common:32.2.1")
-    implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable:2.4.0")
-
-    r8("com.android.tools:r8:9.1.31")
-
-    testImplementation(platform("org.junit:junit-bom:6.1.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api")
-    testImplementation("org.junit.jupiter:junit-jupiter-params")
-    testImplementation("org.junit.platform:junit-platform-launcher")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-
-    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.28.1")
 }
