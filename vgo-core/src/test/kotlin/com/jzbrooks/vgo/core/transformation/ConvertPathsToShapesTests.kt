c7e83dac3402777b9b7fb21c874a10a80afc3c53
Justin Brooks <justin@jzbrooks.com>
2026-08-14 21:01:19 -0400

    Recover compact paths as native shapes

--- vgo-core/src/test/kotlin/com/jzbrooks/vgo/core/transformation/ConvertPathsToShapesTests.kt
+++ vgo-core/src/test/kotlin/com/jzbrooks/vgo/core/transformation/ConvertPathsToShapesTests.kt
@@ -1,12 +1,19 @@
 package com.jzbrooks.vgo.core.transformation
 
+import assertk.all
 import assertk.assertThat
 import assertk.assertions.containsExactly
 import assertk.assertions.hasSize
+import assertk.assertions.index
 import assertk.assertions.isEqualTo
 import assertk.assertions.isInstanceOf
+import assertk.assertions.prop
+import assertk.assertions.single
 import com.jzbrooks.vgo.core.graphic.Circle
+import com.jzbrooks.vgo.core.graphic.Ellipse
+import com.jzbrooks.vgo.core.graphic.Line
 import com.jzbrooks.vgo.core.graphic.Path
+import com.jzbrooks.vgo.core.graphic.Rect
 import com.jzbrooks.vgo.core.graphic.command.CommandString
 import com.jzbrooks.vgo.core.util.element.createGraphic
 import com.jzbrooks.vgo.core.util.element.createPath
@@ -24,8 +31,7 @@ class ConvertPathsToShapesTests {
 
         ConvertPathsToShapes().visit(graphic)
 
-        assertThat(graphic.elements).hasSize(2)
-        assertThat(graphic.elements).containsExactly(
+        assertThat(graphic::elements).containsExactly(
             Circle(
                 null,
                 mutableMapOf(),
@@ -59,11 +65,65 @@ class ConvertPathsToShapesTests {
 
     @Test
     fun `leaves mixed paths unchanged`() {
-        val path = createPath(CommandString("M0,0L10,10").toCommandList())
+        val path = createPath(CommandString("M0,0C2,3,4,5,10,10").toCommandList())
         val graphic = createGraphic(listOf(path))
 
         ConvertPathsToShapes().visit(graphic)
 
-        assertThat(graphic.elements.single()).isInstanceOf<Path>().isEqualTo(path)
+        assertThat(graphic::elements).single().isInstanceOf<Path>().isEqualTo(path)
+    }
+
+    @Test
+    fun `converts merged rectangular subpaths to rectangles`() {
+        val path = createPath(CommandString("M10,20h30v40h-30ZM2,3H8V12H2Z").toCommandList())
+        val graphic = createGraphic(listOf(path))
+
+        ConvertPathsToShapes().visit(graphic)
+
+        assertThat(graphic::elements).hasSize(2)
+        assertThat(graphic::elements).index(0).isInstanceOf<Rect>().all {
+            prop(Rect::x).isEqualTo(10f)
+            prop(Rect::y).isEqualTo(20f)
+            prop(Rect::width).isEqualTo(30f)
+            prop(Rect::height).isEqualTo(40f)
+        }
+        assertThat(graphic::elements).index(1).isInstanceOf<Rect>().all {
+            prop(Rect::x).isEqualTo(2f)
+            prop(Rect::y).isEqualTo(3f)
+            prop(Rect::width).isEqualTo(6f)
+            prop(Rect::height).isEqualTo(9f)
+        }
+    }
+
+    @Test
+    fun `converts ellipse and line subpaths`() {
+        val path = createPath(CommandString("M5,10a5,3,0,1,1,10,0a5,3,0,1,1-10,0M2,3L8,12").toCommandList())
+        val graphic = createGraphic(listOf(path))
+
+        ConvertPathsToShapes().visit(graphic)
+
+        assertThat(graphic::elements).hasSize(2)
+        assertThat(graphic::elements).index(0).isInstanceOf<Ellipse>().all {
+            prop(Ellipse::cx).isEqualTo(10f)
+            prop(Ellipse::cy).isEqualTo(10f)
+            prop(Ellipse::rx).isEqualTo(5f)
+            prop(Ellipse::ry).isEqualTo(3f)
+        }
+        assertThat(graphic::elements).index(1).isInstanceOf<Line>().all {
+            prop(Line::x1).isEqualTo(2f)
+            prop(Line::y1).isEqualTo(3f)
+            prop(Line::x2).isEqualTo(8f)
+            prop(Line::y2).isEqualTo(12f)
+        }
+    }
+
+    @Test
+    fun `keeps path when recovered shapes are rejected`() {
+        val path = createPath(CommandString("M0,0h24v24h-24Z").toCommandList())
+        val graphic = createGraphic(listOf(path))
+
+        ConvertPathsToShapes(shouldConvert = { _, _ -> false }).visit(graphic)
+
+        assertThat(graphic.elements.single()).isEqualTo(path)
     }
 }
